1c1569716521e634106fd105e887512d4a801951
Or Shalmayev <46129649+OrShalmayev@users.noreply.github.com>
2025-02-12 12:12:56 +0200

    refactored let.directive.ts

--- src/app/modules/shared/directives/let/let.directive.ts
+++ src/app/modules/shared/directives/let/let.directive.ts
@@ -1,43 +1,62 @@
 import {
-    Directive,
-    Input,
-    OnInit,
-    TemplateRef,
-    ViewContainerRef
+  Directive,
+  Input,
+  TemplateRef,
+  ViewContainerRef,
+  EmbeddedViewRef
 } from '@angular/core';
 
-class NgLetContext<T> {
-    $implicit: T | undefined;
-    ngLet: T | undefined;
-
-    setData(value: T): void {
-        this.$implicit = value;
-        this.ngLet = value;
-    }
+interface NgLetContext<T> {
+  $implicit: T;
+  ngLet: T;
 }
 
+/**
+ * How It Works:
+ * - The NgLetDirective creates an embedded view from the template it is applied to.
+ * - The context (NgLetContext) is provided to this embedded view, allowing you to access the value via $implicit or ngLet.
+ * - The ngTemplateContextGua rd helps Angular's compiler understand the type of the context, improving type safety within your templates.
+ */
 @Directive({
-    selector: '[ngLet]',
+  selector:   '[ngLet]',
+  standalone: true
 })
-export class NgLetDirective<T = unknown> implements OnInit {
-    private _context = new NgLetContext<T>();
+class NgLetDirective<T = unknown> {
+  private _context: NgLetContext<T> = {
+    $implicit: undefined,
+    ngLet:     undefined
+  };
 
-    @Input('ngLet') set ngLet(value: T) {
-        this._context.setData(value);
-    }
+  private _viewRef: EmbeddedViewRef<NgLetContext<T>> | null = null;
 
-    constructor(
-        private _viewContainer: ViewContainerRef,
-        private templateRef: TemplateRef<NgLetContext<T>>
-    ) {
-    }
+  @Input()
+  set ngLet(value: T) {
+    this._context.$implicit = this._context.ngLet = value;
+    this._updateView();
+  }
 
-    ngOnInit(): void {
-        this._viewContainer.clear();
-        this._viewContainer.createEmbeddedView(this.templateRef, this._context);
-    }
+  constructor(
+    private _viewContainer: ViewContainerRef,
+    private _templateRef: TemplateRef<NgLetContext<T>>
+  ) {
+  }
 
-    static ngTemplateContextGuard<T>(dir: NgLetDirective<T>, ctx: any): ctx is NgLetContext<NonNullable<T>> {
-        return true;
+  private _updateView(): void {
+    if (this._viewRef == null) {
+      this._viewContainer.clear();
+      this._viewRef = this._viewContainer.createEmbeddedView(this._templateRef, this._context);
     }
+    else {
+      this._viewRef.markForCheck();
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  static ngTemplateContextGuard<T>(
+    dir: NgLetDirective<T>,
+    ctx: unknown
+  ): ctx is NgLetContext<NonNullable<T>> {
+    return true;
+  }
 }
+export {NgLetDirective};
